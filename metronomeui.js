6afc601b3657525f40a48d930b9998d814369e33
Joel Kottas <sumnon@cj3.org>
2014-11-15 06:29:46 -0500

    Add UI JS to create def row
    Add a default row to the table successfully but because of position:
    absolute make it overlap with the labels.

--- metronomeui.js
+++ metronomeui.js
@@ -1,7 +1,73 @@
 "use strict";
 
-(function() {
+$(function() {
 
+var rows = [];
+var table = $("#table");
 
+function setToEditFunc() {
+    log("Set to edit");
+    return;
+    setToEdit($(this));
+}
 
-})();
+function toolsFunc() {
+    log("Tools");
+}
+
+function createDefTimeSignature() {
+    return $("<div>")
+        .addClass("cell time-signature")
+        .append($("<span>")
+                .addClass("beats-per-measure")
+                .text("" + defBeatsPerMeasure)
+                .on("click", setToEditFunc))
+        .append($("<span>")
+                .addClass("note-value")
+                .text("" + defNoteValue)
+                .on("click", setToEditFunc));
+}
+
+function createDefBeatsPerMinute() {
+    return $("<div>")
+        .addClass("cell beats-per-minute")
+        .append($("<span>")
+                .text("" + defBPM)
+                .on("click", setToEditFunc));
+}
+
+function createDefNumberOfMeasures() {
+    return $("<div>")
+        .addClass("cell number-of-measures")
+        .append($("<span>")
+                .text("inf")
+                .on("click", setToEditFunc));
+}
+
+function createDefTools() {
+    return $("<div>")
+        .addClass("cell tools")
+        .append($("<button>")
+                .addClass("tools-button")
+                .attr("type", "button")
+                .text("Tools")
+                .on("click", toolsFunc));
+}
+
+function createDefRow() {
+    return $("<div>")
+        .addClass("section")
+        .append(createDefTimeSignature())
+        .append(createDefBeatsPerMinute())
+        .append(createDefNumberOfMeasures())
+        .append(createDefTools());
+}
+
+function appendRow(row) {
+    table.append(row);
+    rows.push(row);
+}
+
+appendRow(createDefRow());
+
+});
